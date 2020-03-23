7e26d5a0484a46f8436941da5d98dcb9f31e1d3b
Scott Shambaugh <scottshambaugh@users.noreply.github.com>
2020-03-22 21:52:24 -0600

    Adding start of MCCase

---
+++ MCCase.py
@@ -0,0 +1,34 @@
+#import datetime
+#from MCVar import MCVar
+#from MCVal import MCVal
+
+class MCCase():
+    def __init__(self, ncase, mcvars):
+        self.ncase = ncase    # ncase is an integer
+        self.mcvars = mcvars  # mcvars is a single instance or tuple of multiple MCVar objects
+        self.starttime = None
+        self.endtime = None
+        self.runtime = None
+        
+        # Package length 1 into an interable tuple
+        if ~isinstance(self.mcvars, tuple):
+            self.mcvars = (self.mcvars,)
+        
+        self.mcvals = self.getMCVals()
+        
+    def getMCVals(self):
+        mcvals = []
+        for mcvar in self.mcvars:
+            mcvals.append(mcvar.getVal(self.ncase))
+        return mcvals
+
+'''
+## Test ##
+import numpy as np
+from scipy.stats import *
+from MCVar import MCVar
+np.random.seed(74494861)
+var = MCVar('Test', norm, (10, 4), 10)
+case = MCCase(0, (var))
+print(case.mcvals[0].val)
+#'''
