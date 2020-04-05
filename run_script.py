0add57a9aaa7dcfecc2e181a09e24ff4f756b785
Scott Shambaugh <scottshambaugh@users.noreply.github.com>
2020-04-05 15:24:29 -0600

    Fix run_script to use correct draws vs cases

--- run_script.py
+++ run_script.py
@@ -6,16 +6,16 @@ from rocket_example_preprocess import rocket_example_preprocess
 from rocket_example_postprocess import rocket_example_postprocess
 from rocket_example_sim import rocket_example_sim
 
-ncases = 5
+ndraws = 5
 seed=123098
 
-sim = MCSim('Rocket', ncases, seed)
+sim = MCSim('Rocket', ndraws, seed)
 
 sim.addInVar('windazi', uniform, (0, 360))
 
 sim.genCases()
 
-for i in range(ncases):
+for i in range(sim.ncases):
     sim_input = rocket_example_preprocess(sim.mccases[i])
     sim_raw_output = rocket_example_sim(*sim_input)
     landing_dist = rocket_example_postprocess(*sim_raw_output)
