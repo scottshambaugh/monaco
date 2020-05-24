7bb31804e0fbfa8f3235fef5228e85ec0352da3c
Scott Shambaugh <scottshambaugh@users.noreply.github.com>
2020-05-24 16:31:25 -0600

    Fix mcvarstat test cases and splitting

--- PyMonteCarlo/MCVar.py
+++ PyMonteCarlo/MCVar.py
@@ -221,7 +221,7 @@ class MCOutVar(MCVar):
                 mcvars[name] = MCOutVar(name=name, vals=vals, ndraws=self.ndraws, \
                                         valmap=self.valmap, firstcaseisnom=self.firstcaseisnom)
                 for mcvarstat in self.mcvarstats:
-                    mcvars[name].addVarStat(p=mcvarstat.p, c=mcvarstat.c, bound=mcvarstat.bound, name=mcvarstat.name)
+                    mcvars[name].addVarStat(stattype=mcvarstat.stattype, statkwargs=mcvarstat.statkwargs, name=mcvarstat.name)
         return mcvars
 
 
@@ -237,7 +237,7 @@ mcinvars['randint'] = MCInVar('randint', randint, (1, 5), 1000, seed=invarseeds[
 print(mcinvars['randint'].stats())
 mcinvars['norm'] = MCInVar('norm', norm, (10, 4), 1000, seed=invarseeds[1])
 print(mcinvars['norm'].stats())
-mcinvars['norm'].addVarStat(p=0.75, c=0.95, bound='2-sided')
+mcinvars['norm'].addVarStat(stattype='orderstatTI', statkwargs={'p':0.75, 'c':0.95, 'bound':'2-sided'})
 print(mcinvars['norm'].mcvarstats[0].vals)
 xk = np.array([1, 5, 6])
 pk = np.ones(len(xk))/len(xk)
@@ -257,7 +257,7 @@ print(mcoutvars['test'].stats())
 
 v = np.array([[1,1],[2,2],[3,3]])
 mcoutvars['test2'] = MCOutVar('test2', [v, v, v, v, v])
-mcoutvars['test2'].addVarStat(p=0.25, c=0.50, bound='1-sided')
+mcoutvars['test2'].addVarStat(stattype='orderstatTI', statkwargs={'p':0.33, 'c':0.50, 'bound':'1-sided'})
 mcoutvars.update(mcoutvars['test2'].split())
 print(mcoutvars['test2 [0]'].nums)
 print(mcoutvars['test2 [0]'].mcvarstats[0].vals)
