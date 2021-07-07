Get integration working for any function values, fix calculation of n_from_error

Monaco/integration_statistics.py
@@ -4,29 +4,33 @@ import numpy as np
 from typing import Union
 from Monaco.gaussian_statistics import pct2sig
 
-def integration_error(isUnderCurve : Union[list[int], list[bool]],  # List of 0 and 1, or False and True values
+def integration_error(nums         : list[float],
                       volume       : float     = 1,  # By default, returns an unscaled error
                       runningError : bool      = False,
                       conf         : float     = 0.95,
                       ) -> Union[float, list[float]]:
     
-    isUnderCurve = np.array([int(x) if x in [True, False] else x for x in isUnderCurve]) # Convert True and False to 1 and 0
-    if set(isUnderCurve) - {0, 1} != set():
-        raise ValueError('isUnderCurve must be a list of either all True/False, or 1/0')
-    
-    n = isUnderCurve.size
+    n = len(nums)
     if n == 1:
-        error1sig = volume*1
+        error1sig = volume
     
-    if not runningError:
-        stdev = np.std(isUnderCurve, ddof=1)
+    elif not runningError:
+        stdev = np.std(nums, ddof=1)
         error1sig = volume*stdev/np.sqrt(n)
     
     else:
-        cummean = np.cumsum(isUnderCurve)/np.arange(1 ,n+1)
-        variances = (cummean - cummean**2)*n/(n-1) # Sample varaince eqn. which holds true only for values of only 0 and 1, where x == x**2
+        # Use Welford's algorithm to calculate the running variance
+        M = np.zeros(n) # Running mean
+        S = np.zeros(n) # Sum of variances
+        M[0] = nums[0]
+        for i in range(1,n):
+            M[i] = M[i-1] + (nums[i]-M[i-1])/(i+1)
+            S[i] = S[i-1] + (nums[i]-M[i-1])*(nums[i]-M[i])
+        variances = np.zeros(n)
+        variances[1:] = S[1:]/np.arange(1, n)
         stdevs = np.sqrt(variances)
-        stdevs[stdevs == 0] = pct2sig(0.5) # Leading zeros will throw off plots, fill with reasonable dummy data
+        
+        stdevs[stdevs == 0] = max(stdevs) # Leading zeros will throw off plots, fill with reasonable dummy data
         error1sig = volume*stdevs/np.sqrt(np.arange(1, n+1))
     
     error = error1sig*pct2sig(conf)
@@ -35,18 +39,33 @@ def integration_error(isUnderCurve : Union[list[int], list[bool]],  # List of 0 
 
 def integration_n_from_err(error  : float,
                            volume : float,
+                           stdev  : float, 
                            conf   : float = 0.95,
-                           stdev  : float = 1, 
                            ) -> int:
     # We generally do not know a-priori what the standard deviation will be, so
-    # stdev defaults to 1 as the max for integration on {0,1}. If a better stdev
-    # is computed on a lower number of cases, that can be subsituted in here to
-    # bootleg a more efficient computation.
+    # best practice is to set to the max range of values on the interval, and 
+    # then calculate a better stdev on a lower number of cases, which can then 
+    # be subsituted in here to bootleg a more efficient computation.
     # Note that sobol sampling should converge much faster than this, at a rate
     # of log(n)^d/n, rather than the 1/sqrt(n) given by random sampling. For 
     # sobol sampling, remember to round n to the next power of 2. 
     # helper_functions.next_power_of_2(n) can help with this 
     
-    n = int(np.ceil((volume*conf*stdev/error)**2))
+    n = int(np.ceil((volume*pct2sig(conf)*stdev/error)**2))
     
     return n
+
+
+def max_variance(low  : float,
+                 high : float,
+                 ) -> float:
+    maxvar = (high-low)**2 / 4
+    return maxvar
+
+
+def max_stdev(low  : float,
+              high : float,
+              ) -> float:
+    maxstd = np.sqrt(max_variance(high=high, low=low))
+    return maxstd
+

Monaco/mc_plot.py
@@ -398,7 +398,7 @@ def mc_plot_integration_convergence(mcoutvar : MCOutVar,
         ax.axhline(refval, color='k')
 
     cummean = volume*np.cumsum(mcoutvar.nums)/np.arange(1, mcoutvar.ncases+1)
-    err = integration_error(isUnderCurve=mcoutvar.nums, volume=volume, runningError=True, conf=conf)
+    err = integration_error(nums=mcoutvar.nums, volume=volume, runningError=True, conf=conf)
     ax.plot(cummean,'r')
     ax.plot(cummean+err, 'b')
     ax.plot(cummean-err, 'b')
@@ -421,7 +421,7 @@ def mc_plot_integration_error(mcoutvar : MCOutVar,
     fig, ax = manage_axis(ax, is3d=False)
 
     cummean = volume*np.cumsum(mcoutvar.nums)/np.arange(1, mcoutvar.ncases+1)
-    err = integration_error(isUnderCurve=mcoutvar.nums, volume=volume, runningError=True, conf=conf)
+    err = integration_error(nums=mcoutvar.nums, volume=volume, runningError=True, conf=conf)
     ax.loglog(err, 'b')
     ax.plot(np.abs(cummean - refval), 'r')
     

examples/integration/integration_example_monte_carlo_sim.py
@@ -1,7 +1,7 @@
 from scipy.stats import uniform
 from Monaco.MCSim import MCSim
 from Monaco.helper_functions import next_power_of_2
-from Monaco.integration_statistics import integration_n_from_err
+from Monaco.integration_statistics import integration_error, integration_n_from_err, max_stdev
 import numpy as np
 
 # Define our functions
@@ -11,6 +11,9 @@ def integration_example_preprocess(mccase):
     return (x, y)
 
 def integration_example_run(x, y):
+    # Note that you can return True/False for simple area integrals (since these
+    # vals automatically get valmapped to the nums 1/0), but best practice is to 
+    # return the value of the function at each sample point.
     isUnderCurve = x**2 + y**2 < 1
     return isUnderCurve
 
@@ -29,17 +32,23 @@ totalArea = (xrange[1] - xrange[0])*(yrange[1] - yrange[0])
 # Maximum Error bound:
 # Sobol sampling will give much faster convergence than random. But for random 
 # sampling, we can determine how many samples we would need to reach an absolute
-# error of 0.01 at 95% confidence
+# error of 0.01 at 95% confidence. 
+# A-priori, we do not know the standard deviation, so best practice is to use 
+# the maximum possible gives the low and high values in your range, and the 
+# a-posteriori error will be better than this since the actual variance is known.
+# You can bootstrap the standard deviation by running a shorter sim and extracting
+# stdev from there
 error = 0.01
 conf = 0.95
-nIfRandom = integration_n_from_err(error=error, volume=totalArea, conf=conf)
-print(f'Number of samples needed to reach an error < {error} if using random sampling: {nIfRandom}')
+stdev = max_stdev(low=0, high=1)
+nIfRandom = integration_n_from_err(error=error, volume=totalArea, stdev=stdev, conf=conf)
+print(f'Number of samples needed to reach an error ≤ ±{error} at {round(conf*100, 2)}% confidence if using random sampling: {nIfRandom}')
 
 # Integration best practices:
-savecasedata = False           # File I/O will crush performance, so recommended not to save case data
-samplemethod = 'sobol'         # Use 'sobol' over 'sobol_random' for a speedup, since all our dists are uniform
-ndraws = next_power_of_2(1e5)  # The sobol methods need to be a power of 2 for best performance and balance
-firstcaseisnom = False         # Since we want a power of 2, we should not run a 'nominal' case which would add 1
+savecasedata = False                 # File I/O will crush performance, so recommended not to save case data
+samplemethod = 'sobol'               # Use 'sobol' over 'sobol_random' for a speedup, since all our dists are uniform
+ndraws = next_power_of_2(nIfRandom)  # The sobol methods need to be a power of 2 for best performance and balance
+firstcaseisnom = False               # Since we want a power of 2, we should not run a 'nominal' case which would add 1
 
 seed=12362397
 
@@ -53,8 +62,9 @@ def integration_example_monte_carlo_sim():
     sim.runSim()
     
     underCurvePct = sum(sim.mcoutvars['pi_est'].nums)/ndraws # Note that (True,False) vals are automatically valmapped to the nums (1,0)
+    err = integration_error(sim.mcoutvars['pi_est'].nums, volume=totalArea, runningError=False, conf=conf)
 
-    resultsstr = f'π ≈ {underCurvePct*totalArea}, n = {ndraws}'
+    resultsstr = f'π ≈ {underCurvePct*totalArea}, n = {ndraws}, {round(conf*100, 2)}% error = ±{err:0.5f}'
     print(resultsstr)
     
     '''
@@ -65,9 +75,9 @@ def integration_example_monte_carlo_sim():
     ax.axis('equal')
     plt.title(resultsstr)
     
-    fig, ax = mc_plot_integration_convergence(sim.mcoutvars['pi_est'], refval = np.pi, volume=totalArea, conf=0.95, title='Approx. value of π')
-    ax.set_ylim((3.1, 3.2))
-    fig, ax = mc_plot_integration_error(sim.mcoutvars['pi_est'], refval = np.pi, volume=totalArea, conf=0.95, title='Approx. error of π')
+    fig, ax = mc_plot_integration_convergence(sim.mcoutvars['pi_est'], refval=np.pi, volume=totalArea, conf=0.95, title='Approx. value of π')
+    ax.set_ylim((3.10, 3.18))
+    fig, ax = mc_plot_integration_error(sim.mcoutvars['pi_est'], refval=np.pi, volume=totalArea, conf=0.95, title='Approx. error of π')
     #'''
     
     return sim

test/test_integration_statistics.py
@@ -1,15 +1,23 @@
 # test_integration_statistics.py
 
 import pytest
-from Monaco.integration_statistics import integration_error, integration_n_from_err
+from Monaco.integration_statistics import integration_error, integration_n_from_err, max_variance, max_stdev
 
 def test_integration_error():
-    assert integration_error([True, False, True])          == pytest.approx(                       0.6533213)
-    assert integration_error([1, 0, 1], runningError=True) == pytest.approx([1.3219756, 0.8486893, 0.6533213])
+    assert integration_error([1, 0, 2], runningError=False, conf=0.95) == pytest.approx(1.1315857)
+    assert integration_error([1, 0, 2], runningError=True , conf=0.95) == pytest.approx([1.9599639, 0.9799819, 1.1315857])
 
 
 def test_integration_n_from_err():
-    assert integration_n_from_err(error=0.01, volume=1, conf=0.95) == 9025
+    assert integration_n_from_err(error=0.01, volume=1, stdev=1, conf=0.95) == 38415
+
+
+def test_max_variance():
+    assert max_variance(low=0, high=1) == pytest.approx(0.25)
+
+
+def test_max_stdev():
+    assert max_stdev(low=0, high=1) == pytest.approx(0.5)
 
 
 ### Inline Testing ###
@@ -19,30 +27,31 @@ if __name__ == '__main__':
     import numpy as np
     import matplotlib.pyplot as plt
 
-    #print(integration_error([0, 2, 3, 1, 2]))
-    print(integration_error([True, False, True]))  # Expected: 0.6533213
-    print(integration_error([1, 0, 1], runningError=True))  # Expected: [1.3219756, 0.8486893, 0.6533213]
-    
-    print(integration_n_from_err(error=0.01, volume=1, conf=0.95))  # Expected: 9025
+    print(integration_error([1, 0, 2])) # Expected: 1.1315857
+    print(integration_error([1, 0, 2], runningError=True, conf=0.95))       # Expected: [1.9599639, 0.9799819, 1.1315857]
+    print(integration_n_from_err(error=0.01, volume=1, stdev=1, conf=0.95)) # Expected: 38415
+    print(max_variance(low=0, high=1))  # Expected: 0.25
+    print(max_stdev(low=0, high=1))     # Expected: 0.5
     
     n = int(1e4)
     conf = 0.95
-    generator = np.random.RandomState(seed=2516214)
-    x = generator.randint(0, 2, size=n)
+    generator = np.random.RandomState(seed=25113604)
+    midpoint = 5
+    x = generator.randint(0, 2*midpoint+1, size=n)
     
     cummean = np.cumsum(x)/np.arange(1 ,n+1)
-    err = integration_error(x, volume=2, runningError=True, conf=0.95)
+    err = integration_error(x, volume=1, runningError=True, conf=0.95)
     plt.figure()
-    plt.hlines(0.5, 0, n, 'k')
+    plt.hlines(midpoint, 0, n, 'k')
     plt.plot(cummean, 'r')
     plt.plot(cummean+err, 'b')
     plt.plot(cummean-err, 'b')
-    plt.ylim((0, 1))
+    plt.ylim((midpoint*0.75, midpoint*1.25))
     plt.ylabel(f'{round(conf*100, 2)}% Confidence Integration Bounds')
     
     plt.figure()
     plt.loglog(err, 'r')
-    plt.plot(np.abs(cummean - 0.5), 'b')
+    plt.plot(np.abs(cummean - midpoint), 'b')
     plt.ylabel(f'{round(conf*100, 2)}% Confidence Absolute Error')
     
 #'''