bc350e45428d45be49022e4f8a046e5a870e9bfd
Scott Shambaugh <scottshambaugh@users.noreply.github.com>
2021-10-04 20:35:47 -0600

    Template updates

---
+++ template/README.md
@@ -0,0 +1,31 @@
+## [monaco](../) - Templates
+
+### Overview
+This directory contains a template you can use as a basis for building your Monte-Carlo simulation. The simulation is of a series of weighted coin flips that comes up 70% heads. By default, the simulation executes 500 draws, and tries to calculate a guess at what the coin's weighting is. These files should be very well commented, and hopefully gives you a sense of how a simulation is set up and run. 
+
+### Running
+After installing `monaco`, download the two python files here and execute `template_monte_carlo_sim.py` in a terminal window. 
+
+### Learning
+Here's some things to try out:
+* Try changing the number of draws to 1000. How does this change the estimated weighting? How about if you change the number of draws to 10000?
+* Try setting `savecasedata` to `False`. How much faster does the simulation run when it doesn't have to write results to file?
+* Change the number of draws to 10 and save the sim to your workspace. Examine some of the data structures under the hood:
+```
+sim = template_monte_carlo_sim()
+sim.__dict__
+sim.mccases[0].__dict__
+sim.mcinvars['flipper'].__dict__
+``` 
+* It turns out that Alex is a cheater and flips the weighted coin, while Sam doesn't know and flips a fair 50-50 coin. Try changing the sim so that it runs this scenario. Hint: try adding a second fair coin as an `InVar`, and changing which gets selected in the `template_preprocess` function based on the flipper. What is the correlation now between the flipper and getting Heads on the coin?
+* Try changing the simulation and the functions so that instead of a weighted coin, you are rolling a 6-sided die.
+* Add more dice as `InVar`s so that you have a total of 5. Edit the postprocessing function to answer the following questions:
+  * What are the odds of a one-roll Yahtzee? (All 5 dice showing the same number)
+  * What is the histogram of the sum of all 5 dice?
+
+<p float="left" align="left">
+<img width="480" height="380" src="https://raw.githubusercontent.com/scottshambaugh/monaco/master/docs/val_var_case_architecture.png">  
+</p>
+
+### Next Steps
+Running through the steps above should start to give you a good sense of how the pieces of the Monte-Carlo simulation fit together, and hopefully the architecture diagram above starts to make sense. Once you're comfortable, head on over to the [examples](../examples) for some more complex simulations in different domains. You'll see that while the simulations have more depth to them, they all follow the same basic structure as this template.
--- template/template_functions.py
+++ template/template_functions.py
@@ -1,10 +1,10 @@
 # This file contains the three functions passed into the fcns dict used by MCSim.
 # It is up to you whether to put all these functions in one file, or break it up
-# into multiple files as the other examples in the examples directory do
+# into multiple files as the other examples in the examples directory do.
 
 
 # The preprocessing function should only take in an MCCase object, and extract the
-# values from inside it in order to build the inputs for the run function
+# values from inside it in order to build the inputs for the run function.
 def template_preprocess(mccase):
     
     # For all random variables that you initialized in your run script and will 
@@ -12,13 +12,13 @@ def template_preprocess(mccase):
     flip = mccase.mcinvals['flip'].val
     flipper = mccase.mcinvals['flipper'].val
     
-    # Import or declare any other unchanging inputs that your function needs as well
+    # Import or declare any other unchanging inputs that your function needs as well.
     coin = 'quarter'
     
-    # Structure your data, and return all the arguments to your sim function packaged in a tuple
-    # This tuple will be unpacked when your sim function is called
+    # Structure your data, and return all the arguments to your sim function 
+    # packaged in a tuple. This tuple will be unpacked when your sim function 
+    # is called.
     return (flip, flipper, coin)
-
 
 
 # The run function input arguments need to match up with the outputs in the unpacked
@@ -26,7 +26,7 @@ def template_preprocess(mccase):
 def template_run(flip, flipper, coin):
     
     # Your simulation calculations will happen here. This function can also be 
-    # a wrapper to call another function or non-python code
+    # a wrapper to call another function or non-python code.
     
     if flip == 0:
         headsortails = 'heads'
@@ -38,14 +38,15 @@ def template_run(flip, flipper, coin):
                          'coin':coin,                  \
                         }
     
-    # The outputs should be returned in a tuple, which will be unpacked when your postprocessing 
-    # function is called
+    # The outputs should be returned in a tuple, which will be unpacked when your 
+    # postprocessing function is called. Note the trailing comma to make this 
+    # tuple iterable.
     return (simulation_output, )
 
 
-
-# All input arguments after mccase need to match up with the outputs in the unpacked
-# tuple from your preprocess function
+# For your postprocessing function, the first argument must be the mccase, and
+# all input arguments after mccase need to match up with the outputs in the unpacked
+# tuple from your run function.
 def template_postprocess(mccase, simulation_output):
     
     # Simulation outputs may be huge, and this is where postprocessing can be
@@ -54,11 +55,11 @@ def template_postprocess(mccase, simulation_output):
     
     # It is good practice to provide a dictionary to map any non-number values
     # to numbers via a known valmap. If needed this will be auto-generated, but
-    # manually assigning numbers will give greater control over plotting
+    # manually assigning numbers will give greater control over plotting.
     valmap = {'heads':0, 'tails':1}
     
     # Add output values from this case's simulation results, case information, 
     # or from other data processing that you do in this file. 
-    # The name supplied here will become the var's name
+    # The name supplied here will become the outvar's name.
     mccase.addOutVal(name='Flip Result', val=simulation_output['headsortails'], valmap=valmap)
     mccase.addOutVal(name='Flip Number', val=mccase.ncase)
