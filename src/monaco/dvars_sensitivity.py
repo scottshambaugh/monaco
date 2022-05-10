9ae3690250cc5d108ce68e44fef481ab4e94ac06
Scott Shambaugh <scottshambaugh@users.noreply.github.com>
2022-05-09 20:08:16 -0600

    Reorder functions

--- src/monaco/dvars_sensitivity.py
+++ src/monaco/dvars_sensitivity.py
@@ -12,277 +12,6 @@ from scipy.optimize import minimize
 from numba import jit
 
 
-def full_states(sim : 'Sim',
-                outvarname : str,
-                ) -> tuple[np.ndarray, np.ndarray]:
-    """
-    Get the full input and output states in combined matrices.
-
-    Parameters
-    ----------
-    sim : monaco.mc_sim.Sim
-        The input simulation.
-    outvarname : str
-        The name of the output variable to calculate sensitivities for. Note
-        that the output variable must be scalar.
-
-    Returns
-    -------
-    X : numpy.ndarray
-        The input variables' percentiles.
-    Y : numpy.ndarray
-        The output variable's nums.
-    """
-    X = np.zeros((sim.ncases, sim.ninvars))
-    Y = np.zeros(sim.ncases)
-    for i, varname in enumerate(sim.invars):
-        X[:, i] = sim.invars[varname].pcts
-    Y[:] = sim.outvars[outvarname].nums
-    return X, Y
-
-
-@jit(nopython=True)
-def calc_rj(hj   : float,
-            phij : float
-            ) -> float:
-    """
-    The covariance function (also called a kernel). We currently use a linear
-    kernel which has a single hyperparameter that must be learned.
-    Note that this is just-in-time compiled by numba for increased speed.
-
-    Parameters
-    ----------
-    hj : float
-        The distance between two state elements.
-    phij : float
-        The hyperparameter for the function.
-
-    Returns
-    -------
-    rj : float
-        The covariance.
-    """
-    rj = max(0, 1 - phij*abs(hj))  # linear covariance function
-    # rj = np.exp(-(abs(hj)/phij))  # exponential covariance function
-    # rj = np.exp(-(hj/phij)**2)  # squared exponential covariance function
-    return rj
-
-
-@jit(nopython=True)
-def calc_Ruw(phi : np.ndarray,
-             Xu  : np.ndarray,
-             Xw  : np.ndarray
-             ) -> float:
-    """
-    Calculate the correlation between two input states.
-    Note that this is just-in-time compiled by numba for increased speed.
-
-    Parameters
-    ----------
-    phi : numpy.ndarray
-        The hyperparameters for the covariance function.
-    Xu : numpy.ndarray
-        The u'th input state.
-    Xw : numpy.ndarray
-        The w'th input state.
-
-    Returns
-    -------
-    Ruw : float
-        The correlation between the two states.
-    """
-    h = Xu - Xw
-    Ruw = 1
-    for j, hj in enumerate(h):
-        Ruw = Ruw*calc_rj(hj, phi[j])
-    return Ruw
-
-
-@jit(nopython=True)
-def calc_R(phi : np.ndarray,
-           X   : np.ndarray,
-           ) -> np.ndarray:
-    """
-    Calculate the correlation matrix between each of the input states.
-    Note that this is just-in-time compiled by numba for increased speed.
-
-    Parameters
-    ----------
-    phi : numpy.ndarray
-        The hyperparameters for the covariance function.
-    X : numpy.ndarray
-        The state matrix for all the input variables percentiles.
-
-    Returns
-    -------
-    R : float
-        The correlation matrix.
-    """
-    m = X.shape[0]
-    R = np.ones((m, m))
-    for u in range(1, m):
-        # do lower triangle only and duplicate across diag
-        # diag will be all 1s
-        for w in range(1, u):
-            Ruw = calc_Ruw(phi, X[u, :], X[w, :])
-            R[u, w] = Ruw
-            R[w, u] = Ruw
-    return R
-
-
-@jit(nopython=True)
-def calc_L(phi : np.ndarray,
-           X   : np.ndarray,
-           Y   : np.ndarray
-           ) -> float:
-    """
-    Calculate the negative log-likelihood cost. Note that this is just-in-time
-    compiled by numba for increased speed.
-
-    Parameters
-    ----------
-    phi : numpy.ndarray
-        The hyperparameters for the covariance function.
-    X : numpy.ndarray
-        The state matrix for all the input variables percentiles.
-    Y : numpy.ndarray
-        The state matrix for the output variables nums.
-
-    Returns
-    -------
-    L : float
-        The negative log-likelihood cost.
-    """
-    m = len(Y)
-    M = np.ones((m, 1))
-    R = calc_R(phi, X)
-    Rinv = np.linalg.inv(R)
-    Rdet = max(np.linalg.det(R), 1e-12)  # Protect for poor conditioning
-
-    mu = np.linalg.inv(M.T @ Rinv @ M) @ (M.T @ Rinv @ Y)
-
-    L_inner = Y - M*mu
-    L = np.log(Rdet)/m + m*np.log(L_inner.T @ Rinv @ L_inner)
-    L = L[0][0]
-    return L
-
-
-def L_runner(phi     : np.ndarray,
-             X       : np.ndarray,
-             Y       : np.ndarray,
-             verbose : bool = False
-             ) -> float:
-    """
-    A wrapper function for calculating the negative log-likelihood cost.
-
-    Parameters
-    ----------
-    phi : numpy.ndarray
-        The hyperparameters for the covariance function.
-    X : numpy.ndarray
-        The state matrix for all the input variables percentiles.
-    Y : numpy.ndarray
-        The state matrix for the output variables nums.
-    verbose : bool
-        Whether to print the values at each step.
-
-    Returns
-    -------
-    L : float
-        The negative log-likelihood cost.
-    """
-    L = calc_L(phi, X, Y)
-    vprint(verbose, f'L = {L:0.4f}, Φ = {phi}')
-    return L
-
-
-def calc_phi_opt(sim        : 'Sim',
-                 outvarname : str,
-                 tol        : float = 1e-6,
-                 ) -> np.ndarray:
-    """
-    Calculate the optimal hyperparameters for the covariance functions between
-    the output variable and each of the input variables via maximum likelihood
-    estimation (MLE). MLE works by minimizing a negative log-likelihood
-    function.
-
-    Parameters
-    ----------
-    sim : monaco.mc_sim.Sim
-        The input simulation.
-    outvarname : str
-        The name of the output variable to calculate sensitivities for. Note
-        that the output variable must be scalar.
-    tol : float, default 1e-6
-        The convergence tolerance for scipy's minimize function acting on the
-        negative log likelihood function.
-
-    Returns
-    -------
-    phi_opt : numpy.ndarray
-        The learned hyperparameters for the covariance functions.
-    """
-    phi_max = 1e6
-    phi_min = 0
-    phi0 = 1
-
-    phi0s = []
-    bounds = []
-    for _ in range(sim.ninvars):
-        phi0s.append(phi0)
-        bounds.append((phi_min, phi_max))
-
-    vprint(sim.verbose, 'Calculating optimal hyperparameters Φ for ' +
-                       f"'{outvarname}' covariances...")
-    X, Y = full_states(sim, outvarname)
-    res = minimize(L_runner, phi0s, args=(X, Y, sim.verbose), bounds=bounds,
-                   tol=tol, method='L-BFGS-B')
-    phi_opt = res.x
-    vprint(sim.verbose, 'Done calculating optimal hyperparameters.')
-
-    return phi_opt
-
-
-def calc_Gammaj(Hj       : float,
-                phij     : float,
-                variance : float
-                ) -> float:
-    """
-    Calculates the IVARS sensitivity index from a learned covariance function.
-
-    This integrates the directional variogram for a specific input variable
-    using trapezoidal integration.
-
-    Parameters
-    ----------
-    Hj : float
-        The fraction of the total parameter space to integrate over. Note that
-        the linear correlation function only has one hyperparameter, so as the
-        Reference notes it is unable to distinguish variogram effects at varying
-        length scales. So, this should not be set to anything other than 1 in
-        practice.
-    phij : float
-        The learned hyperparameter for the covariance function between the
-        output and input variable.
-    variance : float
-        The variance of the output variable.
-
-    Returns
-    -------
-    Gammaj : float
-        The global sensitivity index for this output-input variable pair.
-    """
-    dh = 1e-3
-    q = int(np.floor(Hj/dh))
-    rjs = []
-    for i in range(q+1):
-        rjs.append(calc_rj(dh*i, phij))
-    rjs = np.array(rjs)
-
-    Gammaj = np.trapz(1 - rjs) * dh * variance
-    return Gammaj
-
-
 def calc_sensitivities(sim        : 'Sim',
                        outvarname : str,
                        Hj         : float = 1.0,
@@ -346,3 +75,274 @@ def calc_sensitivities(sim        : 'Sim',
     ratios = sensitivities/sum(sensitivities)
 
     return sensitivities, ratios
+
+
+def calc_phi_opt(sim        : 'Sim',
+                 outvarname : str,
+                 tol        : float = 1e-6,
+                 ) -> np.ndarray:
+    """
+    Calculate the optimal hyperparameters for the covariance functions between
+    the output variable and each of the input variables via maximum likelihood
+    estimation (MLE). MLE works by minimizing a negative log-likelihood
+    function.
+
+    Parameters
+    ----------
+    sim : monaco.mc_sim.Sim
+        The input simulation.
+    outvarname : str
+        The name of the output variable to calculate sensitivities for. Note
+        that the output variable must be scalar.
+    tol : float, default 1e-6
+        The convergence tolerance for scipy's minimize function acting on the
+        negative log likelihood function.
+
+    Returns
+    -------
+    phi_opt : numpy.ndarray
+        The learned hyperparameters for the covariance functions.
+    """
+    phi_max = 1e6
+    phi_min = 0
+    phi0 = 1
+
+    phi0s = []
+    bounds = []
+    for _ in range(sim.ninvars):
+        phi0s.append(phi0)
+        bounds.append((phi_min, phi_max))
+
+    vprint(sim.verbose, 'Calculating optimal hyperparameters Φ for ' +
+                       f"'{outvarname}' covariances...")
+    X, Y = full_states(sim, outvarname)
+    res = minimize(L_runner, phi0s, args=(X, Y, sim.verbose), bounds=bounds,
+                   tol=tol, method='L-BFGS-B')
+    phi_opt = res.x
+    vprint(sim.verbose, 'Done calculating optimal hyperparameters.')
+
+    return phi_opt
+
+
+def full_states(sim : 'Sim',
+                outvarname : str,
+                ) -> tuple[np.ndarray, np.ndarray]:
+    """
+    Get the full input and output states in combined matrices.
+
+    Parameters
+    ----------
+    sim : monaco.mc_sim.Sim
+        The input simulation.
+    outvarname : str
+        The name of the output variable to calculate sensitivities for. Note
+        that the output variable must be scalar.
+
+    Returns
+    -------
+    X : numpy.ndarray
+        The input variables' percentiles.
+    Y : numpy.ndarray
+        The output variable's nums.
+    """
+    X = np.zeros((sim.ncases, sim.ninvars))
+    Y = np.zeros(sim.ncases)
+    for i, varname in enumerate(sim.invars):
+        X[:, i] = sim.invars[varname].pcts
+    Y[:] = sim.outvars[outvarname].nums
+    return X, Y
+
+
+def calc_Gammaj(Hj       : float,
+                phij     : float,
+                variance : float
+                ) -> float:
+    """
+    Calculates the IVARS sensitivity index from a learned covariance function.
+
+    This integrates the directional variogram for a specific input variable
+    using trapezoidal integration.
+
+    Parameters
+    ----------
+    Hj : float
+        The fraction of the total parameter space to integrate over. Note that
+        the linear correlation function only has one hyperparameter, so as the
+        Reference notes it is unable to distinguish variogram effects at varying
+        length scales. So, this should not be set to anything other than 1 in
+        practice.
+    phij : float
+        The learned hyperparameter for the covariance function between the
+        output and input variable.
+    variance : float
+        The variance of the output variable.
+
+    Returns
+    -------
+    Gammaj : float
+        The global sensitivity index for this output-input variable pair.
+    """
+    dh = 1e-3
+    q = int(np.floor(Hj/dh))
+    rjs = []
+    for i in range(q+1):
+        rjs.append(calc_rj(dh*i, phij))
+    rjs = np.array(rjs)
+
+    Gammaj = np.trapz(1 - rjs) * dh * variance
+    return Gammaj
+
+
+def L_runner(phi     : np.ndarray,
+             X       : np.ndarray,
+             Y       : np.ndarray,
+             verbose : bool = False
+             ) -> float:
+    """
+    A wrapper function for calculating the negative log-likelihood cost.
+
+    Parameters
+    ----------
+    phi : numpy.ndarray
+        The hyperparameters for the covariance function.
+    X : numpy.ndarray
+        The state matrix for all the input variables percentiles.
+    Y : numpy.ndarray
+        The state matrix for the output variables nums.
+    verbose : bool
+        Whether to print the values at each step.
+
+    Returns
+    -------
+    L : float
+        The negative log-likelihood cost.
+    """
+    L = calc_L(phi, X, Y)
+    vprint(verbose, f'L = {L:0.4f}, Φ = {phi}')
+    return L
+
+
+@jit(nopython=True)
+def calc_L(phi : np.ndarray,
+           X   : np.ndarray,
+           Y   : np.ndarray
+           ) -> float:
+    """
+    Calculate the negative log-likelihood cost. Note that this is just-in-time
+    compiled by numba for increased speed.
+
+    Parameters
+    ----------
+    phi : numpy.ndarray
+        The hyperparameters for the covariance function.
+    X : numpy.ndarray
+        The state matrix for all the input variables percentiles.
+    Y : numpy.ndarray
+        The state matrix for the output variables nums.
+
+    Returns
+    -------
+    L : float
+        The negative log-likelihood cost.
+    """
+    m = len(Y)
+    M = np.ones((m, 1))
+    R = calc_R(phi, X)
+    Rinv = np.linalg.inv(R)
+    Rdet = max(np.linalg.det(R), 1e-12)  # Protect for poor conditioning
+
+    mu = np.linalg.inv(M.T @ Rinv @ M) @ (M.T @ Rinv @ Y)
+
+    L_inner = Y - M*mu
+    L = np.log(Rdet)/m + m*np.log(L_inner.T @ Rinv @ L_inner)
+    L = L[0][0]
+    return L
+
+
+@jit(nopython=True)
+def calc_R(phi : np.ndarray,
+           X   : np.ndarray,
+           ) -> np.ndarray:
+    """
+    Calculate the correlation matrix between each of the input states.
+    Note that this is just-in-time compiled by numba for increased speed.
+
+    Parameters
+    ----------
+    phi : numpy.ndarray
+        The hyperparameters for the covariance function.
+    X : numpy.ndarray
+        The state matrix for all the input variables percentiles.
+
+    Returns
+    -------
+    R : float
+        The correlation matrix.
+    """
+    m = X.shape[0]
+    R = np.ones((m, m))
+    for u in range(1, m):
+        # do lower triangle only and duplicate across diag
+        # diag will be all 1s
+        for w in range(1, u):
+            Ruw = calc_Ruw(phi, X[u, :], X[w, :])
+            R[u, w] = Ruw
+            R[w, u] = Ruw
+    return R
+
+
+@jit(nopython=True)
+def calc_Ruw(phi : np.ndarray,
+             Xu  : np.ndarray,
+             Xw  : np.ndarray
+             ) -> float:
+    """
+    Calculate the correlation between two input states.
+    Note that this is just-in-time compiled by numba for increased speed.
+
+    Parameters
+    ----------
+    phi : numpy.ndarray
+        The hyperparameters for the covariance function.
+    Xu : numpy.ndarray
+        The u'th input state.
+    Xw : numpy.ndarray
+        The w'th input state.
+
+    Returns
+    -------
+    Ruw : float
+        The correlation between the two states.
+    """
+    h = Xu - Xw
+    Ruw = 1
+    for j, hj in enumerate(h):
+        Ruw = Ruw*calc_rj(hj, phi[j])
+    return Ruw
+
+
+@jit(nopython=True)
+def calc_rj(hj   : float,
+            phij : float
+            ) -> float:
+    """
+    The covariance function (also called a kernel). We currently use a linear
+    kernel which has a single hyperparameter that must be learned.
+    Note that this is just-in-time compiled by numba for increased speed.
+
+    Parameters
+    ----------
+    hj : float
+        The distance between two state elements.
+    phij : float
+        The hyperparameter for the function.
+
+    Returns
+    -------
+    rj : float
+        The covariance.
+    """
+    rj = max(0, 1 - phij*abs(hj))  # linear covariance function
+    # rj = np.exp(-(abs(hj)/phij))  # exponential covariance function
+    # rj = np.exp(-(hj/phij)**2)  # squared exponential covariance function
+    return rj
