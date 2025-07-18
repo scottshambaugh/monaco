6e220df93cda59b4c8694788c8469cc70fbcfe75
Scott Shambaugh <scottshambaugh@users.noreply.github.com>
2025-07-18 09:54:36 -0600

    Clean up test_mc_sim fixturing

--- tests/test_mc_sim.py
+++ tests/test_mc_sim.py
@@ -19,6 +19,13 @@ try:
 except ImportError:
     HAS_PANDAS = False
 
+SIM_FIXTURES = [
+    "sim_singlethreaded",
+    "sim_multiprocessing",
+    "sim_multiprocessing_expanded",
+    "sim_dask",
+    "sim_dask_expanded"
+]
 
 def sim_testing_preprocess(case):
     return ([case.ncase, ])
@@ -133,15 +140,9 @@ def sim_dask_expanded(sim_base):
 
 
 @pytest.fixture
-def all_sims(sim_singlethreaded,
-             sim_multiprocessing, sim_multiprocessing_expanded,
-             sim_dask, sim_dask_expanded):
-    all_sims = [sim_singlethreaded,
-                sim_multiprocessing, sim_multiprocessing_expanded,
-                sim_dask, sim_dask_expanded]
-    # We only test the dask sims if dask is installed
-    sims_downselect = [sim for sim in all_sims if sim is not None]
-    return sims_downselect
+def sim_fixture(request):
+    """Fixture that provides specific sim fixtures based on parametrization"""
+    return request.getfixturevalue(request.param)
 
 
 def test_sim_singlethreaded_fixture(sim_singlethreaded):
@@ -173,33 +174,39 @@ def test_sim_getitem(sim_singlethreaded):
     assert sim_singlethreaded['Var1'].vals[0] == pytest.approx(3)
 
 
-def test_sim_dist_draws(all_sims):
-    for sim in all_sims:
-        assert sim.cases[0].invals['Var1'].val == pytest.approx(3)
-        assert sim.cases[1].invals['Var2'].val == pytest.approx(9.98228884)
+@pytest.mark.parametrize("sim_fixture", SIM_FIXTURES, indirect=True)
+def test_sim_dist_draws(sim_fixture):
+    if sim_fixture is None:
+        pytest.skip("Dask is not installed, skipping parallel tests")
+
+    assert sim_fixture.cases[0].invals['Var1'].val == pytest.approx(3)
+    assert sim_fixture.cases[1].invals['Var2'].val == pytest.approx(9.98228884)
 
 
-def test_sim_scalaroutvars(all_sims):
-    for sim in all_sims:
-        assert len(sim.scalarOutVars()) == 1
+@pytest.mark.parametrize("sim_fixture", SIM_FIXTURES, indirect=True)
+def test_sim_scalaroutvars(sim_fixture):
+    if sim_fixture is None:
+        pytest.skip("Dask is not installed, skipping parallel tests")
+
+    assert len(sim_fixture.scalarOutVars()) == 1
 
 
-def test_sim_extendoutvars(all_sims):
-    for sim in all_sims:
-        assert len(sim.outvars['casenum_list'][0].val) == 1
-        assert len(sim.outvars['casenum_list'][0].num) == 1
-        assert len(sim.outvars['casenum_array'][0].val) == 1
+def test_sim_extendoutvars(sim_singlethreaded):
+    sim = sim_singlethreaded
+    assert len(sim.outvars['casenum_list'][0].val) == 1
+    assert len(sim.outvars['casenum_list'][0].num) == 1
+    assert len(sim.outvars['casenum_array'][0].val) == 1
+    if HAS_PANDAS:
+        assert len(sim.outvars['casenum_series'][0].val) == 1
+        assert len(sim.outvars['casenum_index'][0].val) == 1
+    sim.extendOutVars()
+    for i in range(sim.ncases):
+        assert len(sim.outvars['casenum_list'][i].val) == sim.ncases
+        assert len(sim.outvars['casenum_list'][i].num) == sim.ncases
+        assert len(sim.outvars['casenum_array'][i].val) == sim.ncases
         if HAS_PANDAS:
-            assert len(sim.outvars['casenum_series'][0].val) == 1
-            assert len(sim.outvars['casenum_index'][0].val) == 1
-        sim.extendOutVars()
-        for i in range(sim.ncases):
-            assert len(sim.outvars['casenum_list'][i].val) == sim.ncases
-            assert len(sim.outvars['casenum_list'][i].num) == sim.ncases
-            assert len(sim.outvars['casenum_array'][i].val) == sim.ncases
-            if HAS_PANDAS:
-                assert len(sim.outvars['casenum_series'][i].val) == sim.ncases
-                assert len(sim.outvars['casenum_index'][i].val) == sim.ncases
+            assert len(sim.outvars['casenum_series'][i].val) == sim.ncases
+            assert len(sim.outvars['casenum_index'][i].val) == sim.ncases
 
 
 def test_sim_custom_vals():
@@ -222,74 +229,90 @@ def test_sim_custom_vals():
                      vals=[1, 2, 3])
 
 
-def test_sim_corr_cov(all_sims):
-    for sim in all_sims:
-        # Convert to numpy arrays because pytest.approx doesn't work on nested lists
-        assert np.array(sim.corr()[0]) \
-            == pytest.approx(np.array([[ 1,           0.44720757, -0.18114221],
-                                       [ 0.44720757,  1,          -0.01869903],
-                                       [-0.18114221, -0.01869903,  1]]))
-        assert np.array(sim.cov()[0]) \
-            == pytest.approx(np.array([[2.05882353,  2.44162274, -1.3125],
-                                       [2.44162274, 14.47837096, -0.35929331],
-                                       [-1.3125   , -0.35929331, 25.5]]))
+@pytest.mark.parametrize("sim_fixture", SIM_FIXTURES, indirect=True)
+def test_sim_corr_cov(sim_fixture):
+    if sim_fixture is None:
+        pytest.skip("Dask is not installed, skipping parallel tests")
+
+    sim = sim_fixture
+    # Convert to numpy arrays because pytest.approx doesn't work on nested lists
+    assert np.array(sim.corr()[0]) \
+        == pytest.approx(np.array([[ 1,           0.44720757, -0.18114221],
+                                   [ 0.44720757,  1,          -0.01869903],
+                                   [-0.18114221, -0.01869903,  1]]))
+    assert np.array(sim.cov()[0]) \
+        == pytest.approx(np.array([[2.05882353,  2.44162274, -1.3125],
+                                   [2.44162274, 14.47837096, -0.35929331],
+                                   [-1.3125   , -0.35929331, 25.5]]))
 
 
-def test_sim_preprocess_failure(all_sims):
-    for sim in all_sims:
-        fcns = {SimFunctions.PREPROCESS : sim_testing_preprocess_failure,
-                SimFunctions.RUN        : sim_testing_run,
-                SimFunctions.POSTPROCESS: sim_testing_postprocess}
-        sim.fcns = fcns
-        sim.debug = True
-        sim.clearResults()
-        with pytest.raises(Exception, match='Preprocess testing failed for case 0'):
-            sim.runSim()
+@pytest.mark.parametrize("sim_fixture", SIM_FIXTURES, indirect=True)
+def test_sim_preprocess_failure(sim_fixture):
+    if sim_fixture is None:
+        pytest.skip("Dask is not installed, skipping parallel tests")
 
-        sim.debug = False
-        sim.clearResults()
+    sim = sim_fixture
+    fcns = {SimFunctions.PREPROCESS : sim_testing_preprocess_failure,
+            SimFunctions.RUN        : sim_testing_run,
+            SimFunctions.POSTPROCESS: sim_testing_postprocess}
+    sim.fcns = fcns
+    sim.debug = True
+    sim.clearResults()
+    with pytest.raises(Exception, match='Preprocess testing failed for case 0'):
         sim.runSim()
-        assert len(sim.casespreprocessed) == sim.ncases - 1
-        assert len(sim.casesrun) == sim.ncases - 1
-        assert len(sim.casespostprocessed) == sim.ncases - 1
+
+    sim.debug = False
+    sim.clearResults()
+    sim.runSim()
+    assert len(sim.casespreprocessed) == sim.ncases - 1
+    assert len(sim.casesrun) == sim.ncases - 1
+    assert len(sim.casespostprocessed) == sim.ncases - 1
 
 
-def test_sim_run_failure(all_sims):
-    for sim in all_sims:
-        fcns = {SimFunctions.PREPROCESS : sim_testing_preprocess,
-                SimFunctions.RUN        : sim_testing_run_failure,
-                SimFunctions.POSTPROCESS: sim_testing_postprocess}
-        sim.fcns = fcns
-        sim.debug = True
-        sim.clearResults()
-        with pytest.raises(Exception, match='Run testing failed for case 0'):
-            sim.runSim()
+@pytest.mark.parametrize("sim_fixture", SIM_FIXTURES, indirect=True)
+def test_sim_run_failure(sim_fixture):
+    if sim_fixture is None:
+        pytest.skip("Dask is not installed, skipping parallel tests")
 
-        sim.debug = False
-        sim.clearResults()
+    sim = sim_fixture
+    fcns = {SimFunctions.PREPROCESS : sim_testing_preprocess,
+            SimFunctions.RUN        : sim_testing_run_failure,
+            SimFunctions.POSTPROCESS: sim_testing_postprocess}
+    sim.fcns = fcns
+    sim.debug = True
+    sim.clearResults()
+    with pytest.raises(Exception, match='Run testing failed for case 0'):
         sim.runSim()
-        assert len(sim.casespreprocessed) == sim.ncases
-        assert len(sim.casesrun) == sim.ncases - 1
-        assert len(sim.casespostprocessed) == sim.ncases - 1
+
+    sim.debug = False
+    sim.clearResults()
+    sim.runSim()
+    assert len(sim.casespreprocessed) == sim.ncases
+    assert len(sim.casesrun) == sim.ncases - 1
+    assert len(sim.casespostprocessed) == sim.ncases - 1
 
 
-def test_sim_postprocess_failure(all_sims):
-    for sim in all_sims:
-        fcns = {SimFunctions.PREPROCESS : sim_testing_preprocess,
-                SimFunctions.RUN        : sim_testing_run,
-                SimFunctions.POSTPROCESS: sim_testing_postprocess_failure}
-        sim.fcns = fcns
-        sim.debug = True
-        sim.clearResults()
-        with pytest.raises(Exception, match='Postprocess testing failed for case 0'):
-            sim.runSim()
+@pytest.mark.parametrize("sim_fixture", SIM_FIXTURES, indirect=True)
+def test_sim_postprocess_failure(sim_fixture):
+    if sim_fixture is None:
+        pytest.skip("Dask is not installed, skipping parallel tests")
 
-        sim.debug = False
-        sim.clearResults()
+    sim = sim_fixture
+    fcns = {SimFunctions.PREPROCESS : sim_testing_preprocess,
+            SimFunctions.RUN        : sim_testing_run,
+            SimFunctions.POSTPROCESS: sim_testing_postprocess_failure}
+    sim.fcns = fcns
+    sim.debug = True
+    sim.clearResults()
+    with pytest.raises(Exception, match='Postprocess testing failed for case 0'):
         sim.runSim()
-        assert len(sim.casespreprocessed) == sim.ncases
-        assert len(sim.casesrun) == sim.ncases
-        assert len(sim.casespostprocessed) == sim.ncases - 1
+
+    sim.debug = False
+    sim.clearResults()
+    sim.runSim()
+    assert len(sim.casespreprocessed) == sim.ncases
+    assert len(sim.casesrun) == sim.ncases
+    assert len(sim.casespostprocessed) == sim.ncases - 1
 
 
 # Does not test the plot appearances, but does check that the codepaths can run
